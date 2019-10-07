e0227440424953ff3ec72d6912d39084cc889d99
GuoYQ <172544714@qq.com>
2019-10-07 17:23:47 +0800

    过滤器配置，持久层扫描的包修改

--- GoodBiz/src/main/java/dong/GW/list/service/ClientService.java
+++ GoodBiz/src/main/java/dong/GW/list/service/ClientService.java
@@ -1,4 +1,8 @@
 package dong.GW.list.service;
 
+import dong.GW.list.Entity.Client;
+
 public interface ClientService {
+    void test();
+    void editinfo(Client client);
 }
--- GoodBiz/src/main/java/dong/GW/list/serviceimpl/ClientServiceImpl.java
+++ GoodBiz/src/main/java/dong/GW/list/serviceimpl/ClientServiceImpl.java
@@ -1,4 +1,35 @@
 package dong.GW.list.serviceimpl;
 
-public class ClientServiceImpl {
+import dong.GW.list.Entity.Client;
+import dong.GW.list.Entity.ClientExample;
+import dong.GW.list.Mapper.ClientMapper;
+import dong.GW.list.service.ClientService;
+import javafx.scene.shape.Path;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:spring-biz.xml"})
+public class ClientServiceImpl implements ClientService {
+
+
+    @Autowired
+    private ClientMapper clientMapper;
+
+    @Test
+    public void test() {
+        Client cc = new Client();
+        cc.setId(2);
+        ClientExample ce = new ClientExample();
+        clientMapper.selectByPrimaryKey(cc.getId());
+//        System.out.println(cc.getNickname());
+    }
+
+    public void editinfo(Client client) {
+
+    }
+
 }
